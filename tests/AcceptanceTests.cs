b8b22ef870239f5b3a9d4a01b85057082d3d67a4
elcorbs <emma@madetech.com>
2020-03-31 11:02:13 +0100

    First acceptance test

--- tests/AcceptanceTests.cs
+++ tests/AcceptanceTests.cs
@@ -1,20 +1,38 @@
+using System;
+using System.IO;
+using FluentAssertions;
 using NUnit.Framework;
+using src;
 
 namespace tests
 {
     public class Tests
     {
+        private StringWriter _textWriter;
+        private TextWriter _tmp;
+
         [SetUp]
         public void Setup()
         {
+            _tmp = Console.Out;
+            _textWriter = new StringWriter();
+            Console.SetOut(_textWriter);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.SetOut(_tmp);
         }
 
         [Test]
         public void EmptyBoardDisplayedAtTheBeginningOfAGame()
         {
-            var emptyBoard = "   " +
-                             "   " +
-                             "   ";
+            const string emptyBoard = "   " +
+                                      "   " +
+                                      "   ";
+            Program.Main(new string[]{});
+            _textWriter.ToString().Should().Be(emptyBoard);
         }
     }
 }
